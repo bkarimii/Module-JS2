351c95f8ff0a2e657ff88e240c734de7c371df8c
karimi <behrouzkarimi1993@gmail.com>
2023-11-21 12:54:45 +0000

    set up html and adding forward and backward button

--- week-3/slideshow/slideshow.js
+++ week-3/slideshow/slideshow.js
@@ -1,8 +1,43 @@
-const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
-];
+
+document.addEventListener('DOMContentLoaded',function(){
+    const images = [
+        './animal/cat.jpg',
+        "./animal/coala.jpeg",
+        "./animal/dog.jpg",
+        './animal/panda.jpg'
+    ];
+    let counter=0;
+
+    const img=document.getElementById('carousel-img');
+    img.setAttribute('src',images[counter]);
+
+    const forwardBtn=document.getElementById("forward-btn");
+    forwardBtn.addEventListener('click',function(){
+        if(counter===3){
+            counter=0;
+            img.setAttribute('src',images[counter]);
+            counter++;
+        }
+        else{
+            counter++;
+            img.setAttribute('src',images[counter]);
+        }
+    })
+
+    const backWard=document.getElementById("backward-btn")
+    backWard.addEventListener('click',function(){
+        if(counter===0){
+            counter=3;
+            img.setAttribute('src',images[counter]);
+            counter--;
+        }
+        else{
+            counter--;
+            img.setAttribute('src',images[counter]);
+        }
+    })
+})
+
 
 
 // Write your code here
